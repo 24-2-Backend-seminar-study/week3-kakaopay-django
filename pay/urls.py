22821c9d1179fc376bc9389f6ead1e5357c33f93
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-26 17:33:35 +0900

    feat: add approve url

--- pay/urls.py
+++ pay/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PayReadyView
+from .views import PayReadyView, PayApproveView
 
 
 app_name = "pay"
 urlpatterns = [
     # CBV url path
     path("ready/", PayReadyView.as_view()),
+    path("approve/", PayApproveView.as_view()),
 ]
